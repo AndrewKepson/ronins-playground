987ef6d87aa037a4a5862d2e4de086e1f36542e3
AndrewKepson <60700164+AndrewKepson@users.noreply.github.com>
2022-03-02 13:41:57 -0700

    Adds js

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var button = document.getElementById('button');
+var lowerSection = document.querySelector('.lower');
+
+function click() {
+    alert ('Thanks for clicking me')
+
+}
+
+button.addEventListener('click', click);
